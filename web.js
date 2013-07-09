693800e25fa1289c2250d258e4e5375cc1e7bedb
Dan Sexton <notxesnad@gmail.com>
2013-07-09 16:45:54 +0000

    upadate web.js

--- web.js
+++ web.js
@@ -1,15 +1,20 @@
 var fs = require('fs')
 
 var index = fs.readFileSync('index.html');
-var buffer = new Buffer(16);
-buffer.write(index);
-var index2 = buffer.toString("utf-8");
+//var buffer = new Buffer(16);
+//buffer.write(index);
+//var index2 = buffer.toString("utf-8");
+buf = new Buffer(256);
+len = buf.write(index, 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send( index2 );
+  response.send(len );
 });
 
 var port = process.env.PORT || 5000;

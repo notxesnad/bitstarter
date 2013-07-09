4668e12fe2c7f29466798e2f059afc5976fafde9
Dan Sexton <notxesnad@gmail.com>
2013-07-09 02:37:35 +0000

    updated web.js and index.hmtl

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs')
+
+var index = fs.readFileSync('index.html');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World2!' + index );
 });
 
 var port = process.env.PORT || 5000;

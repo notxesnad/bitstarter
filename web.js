2fd3c1863538cccded68c02ed392d067e23c999b
Dan Sexton <notxesnad@gmail.com>
2013-07-09 16:59:20 +0000

    hopefully the last one

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
 var fs = require('fs')
 
 var index = fs.readFileSync('index.html', 'utf-8');
-//var buffer = new Buffer(16);
-//buffer.write(index);
-//var index2 = buffer.toString("utf-8");
-//buf = new Buffer(256);
-//len = buf.write(index, 0);
-//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 
+var index2 = fs.readFileSync('index.html');
+
+buf = new Buffer(256);
+tindex = buf.write(index2,0);
+tindex2 = buf.toString('utf-8',0, tindex);
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index );
+  response.send(index + tindex2 );
 });
 
 var port = process.env.PORT || 5000;

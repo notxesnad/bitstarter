1c64559acece199457007f5c5002ae43e8884d40
Dan Sexton <notxesnad@gmail.com>
2013-07-09 03:10:48 +0000

    added buffer to web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var fs = require('fs')
 
 var index = fs.readFileSync('index.html');
-
+var buffer = new Buffer(16);
+buffer.write(index , "utf-8");
+var index2 = buffer.toString("utf-8");
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!' + index );
+  response.send( index2 );
 });
 
 var port = process.env.PORT || 5000;
